6e6dbc80bfea05dc0ef832fa711bbd9bdc992a9c
parvej3300 <67873053+parvej3300@users.noreply.github.com>
2025-09-19 10:24:52 +0000

    Stack and heap memory

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -35,3 +35,21 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+/*=====================================*/
+// Stack Memory (Primitive), Heap Memory (non-Primitive)
+let name1 = "Parvej"
+anothername1 = name1
+name1 = "hello testing"
+// console.log(anothername1)
+// console.log(name1)
+
+let user1 = {
+    email: "test@gmail.com",
+    upi: "test@ybl"
+}
+
+let user2 = user1
+user2.email = "test@google.com"
+console.log(user1)
+console.log(user2)
